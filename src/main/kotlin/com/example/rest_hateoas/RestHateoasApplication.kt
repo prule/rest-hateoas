74b51e244fb640d7ffafaeda7e9360b6f739475b
paulrule <paulrule1@gmail.com>
2024-07-23 14:12:44 +1000

    initial commit - created from https://start.spring.io/#!type=maven-project&groupId=com.example&artifactId=rest-hateoas&name=rest-hateoas&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.rest-hateoas&dependencies=hateoas

---
+++ src/main/kotlin/com/example/rest_hateoas/RestHateoasApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rest_hateoas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestHateoasApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestHateoasApplication>(*args)
+}
