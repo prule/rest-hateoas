03c9b6933eecd121d688bfc110e89be81f5ec69d
paulrule <paulrule1@gmail.com>
2024-08-21 11:17:06 +1000

    set up asciidoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
+import org.asciidoctor.gradle.jvm.pdf.AsciidoctorPdfTask
+
+
 //https://spring.io/guides/tutorials/spring-boot-kotlin
 plugins {
     id("org.springframework.boot") version "3.3.2"
@@ -8,6 +12,18 @@ plugins {
     kotlin("plugin.allopen") version "1.9.24"
     kotlin("kapt") version "1.9.24"
     kotlin("plugin.serialization") version "1.9.24"
+
+    alias(libs.plugins.asciidoctor.pdf)
+    alias(libs.plugins.asciidoctor.convert)
+    alias(libs.plugins.asciidoctor.epub)
+    alias(libs.plugins.asciidoctor.gems)
+}
+
+repositories {
+    mavenCentral()
+    ruby {
+        gems()
+    }
 }
 
 allOpen {
@@ -28,6 +44,34 @@ java {
 repositories {
     mavenCentral()
 }
+
+tasks.register("generateDocs") {
+    dependsOn("asciidoctor", "asciidoctorPdf")
+    group = "documentation"
+    description = "Generates both HTML and PDF documentation"
+}
+
+// tag::asciidoctor-gradle-configuration[]
+tasks {
+
+    val asciidocAttributes = mapOf(
+        // define a custom attribute to be used in the document eg as {source}
+        // unfortunately these won't work in the intellij preview, only in the gradle output
+        // so you would need to separately define these attributes in the intellij settings
+        "main_source" to project.sourceSets.main.get().kotlin.srcDirs.first(),
+        "test_source" to project.sourceSets.test.get().kotlin.srcDirs.first(),
+    )
+
+    "asciidoctor"(AsciidoctorTask::class) {
+        baseDirFollowsSourceDir()
+        attributes(asciidocAttributes)
+    }
+    "asciidoctorPdf"(AsciidoctorPdfTask::class) {
+        baseDirFollowsSourceDir()
+        attributes(asciidocAttributes)
+    }
+}
+// end::asciidoctor-gradle-configuration[]
 
 dependencies {
 
