3542848822cec4c3cc029e312c24a42f64800fd6
paulrule <paulrule1@gmail.com>
2024-09-03 17:50:44 +1000

    convert to ports and adapters - move classes to proper location

--- adapter/src/main/kotlin/com/example/rest_hateoas/adapter/in/rest/person/PersonCreateRestModel.kt
+++ adapter/src/main/kotlin/com/example/rest_hateoas/adapter/in/rest/person/PersonCreateRestModel.kt
@@ -1,14 +1,15 @@
 package com.example.rest_hateoas.adapter.`in`.rest.person
 
 import com.example.rest_hateoas.adapter.`in`.rest.support.http.LocalDateSerializer
+import jakarta.validation.Valid
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
 
 @Serializable
 open class PersonCreateRestModel(
 
-    var name: PersonNameRestModel,
-    var address: PersonAddressRestModel,
+    @Valid var name: PersonNameRestModel,
+    @Valid var address: PersonAddressRestModel,
 
     @Serializable(with = LocalDateSerializer::class)
     var dateOfBirth: LocalDate? = null
--- bootstrap/src/main/kotlin/com/example/rest_hateoas/bootstrap/Launcher.kt
+++ bootstrap/src/main/kotlin/com/example/rest_hateoas/bootstrap/Launcher.kt
@@ -3,11 +3,6 @@ package com.example.rest_hateoas.bootstrap
 import com.example.rest_hateoas.adapter.RestHateoasApplication
 import org.springframework.boot.SpringApplication
 
-/**
- * Launcher for the application: starts the Spring application.
- *
- * @author Sven Woltmann
- */
 object Launcher {
     @JvmStatic
     fun main(args: Array<String>) {
