3d9d4053c2cc6cedcb55ceb452d21d4c11527ee7
paulrule <paulrule1@gmail.com>
2024-09-12 10:19:47 +1000

    add openapi annotations

--- adapter/src/main/kotlin/com/example/rest_hateoas/adapter/in/rest/person/PersonFindController.kt
+++ adapter/src/main/kotlin/com/example/rest_hateoas/adapter/in/rest/person/PersonFindController.kt
@@ -2,6 +2,10 @@ package com.example.rest_hateoas.adapter.`in`.rest.person
 
 import com.example.rest_hateoas.application.domain.model.Key
 import com.example.rest_hateoas.application.port.`in`.PersonFindUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +20,17 @@ class PersonFindController(
     private val personRestMapper: PersonRestMapper
 ) {
 
+    @Operation(summary = "Find person by key", description = "Find person by key", tags = ["Person"])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Found Person",
+                content = [(Content(mediaType = "application/json"))]
+            ),
+            ApiResponse(responseCode = "400", description = "Bad request", content = [Content()]),
+            ApiResponse(responseCode = "404", description = "Did not find person with given key", content = [Content()])]
+    )
     @GetMapping("/api/1/persons/{key}")
     fun find(
         @PathVariable(required = true) key: String,
